66dcc8f527a7dc572fce9532bea343df69573f0e
Karol Schnur <29566494+krakong@users.noreply.github.com>
2020-03-21 10:17:32 +0100

    Add creation of LocationHistory objects

--- backend/src/main/java/de/wirvsvirus/kek/service/locations/controller/UploadLocationDataController.java
+++ backend/src/main/java/de/wirvsvirus/kek/service/locations/controller/UploadLocationDataController.java
@@ -1,10 +1,13 @@
 package de.wirvsvirus.kek.service.locations.controller;
 
 import de.wirvsvirus.kek.service.diary.model.Diary;
-import de.wirvsvirus.kek.service.locations.model.PlaceVisitModel;
+import de.wirvsvirus.kek.service.diary.repository.DiaryRepository;
 import de.wirvsvirus.kek.service.locations.model.pojo.TimeLineObject;
 import de.wirvsvirus.kek.service.locations.model.pojo.TimelineJsonRoot;
+import de.wirvsvirus.kek.service.locations.repository.LocationHistory;
+import de.wirvsvirus.kek.service.locations.repository.LocationHistoryRepository;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,32 +16,32 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/")
 public class UploadLocationDataController {
 
+    @Autowired
+    private LocationHistoryRepository locationHistoryRepository;
+
     @PostMapping("/locations/{user}/upload")
     @ApiOperation(value = "Responds with a list of diaries, if parameters are set it will respond with a list of contacts taken between start and finish", response = Diary.class)
     public ResponseEntity<String> uploadLocationData(@RequestBody TimelineJsonRoot jsonData, @PathVariable String user) {
-        List<PlaceVisitModel> visitedPlaces = jsonData.getTimelineObjects().stream()
+        jsonData.getTimelineObjects().stream()
                 .filter( timeLineObject -> timeLineObject.getPlaceVisit() != null)
-                .map( timeLineObject -> toModel(timeLineObject, user))
-                .collect(Collectors.toList());
-
+                .forEach(this::extractData);
 
         return new ResponseEntity<String>("Upload success", HttpStatus.OK);
     }
 
-    private PlaceVisitModel toModel(TimeLineObject timeLineObject, String user) {
-        PlaceVisitModel placeVisitModel = new PlaceVisitModel();
-        placeVisitModel.setUser(user);
-        placeVisitModel.setCenterLatE7(timeLineObject.getPlaceVisit().getCenterLatE7());
-        placeVisitModel.setCenterLngE7(timeLineObject.getPlaceVisit().getCenterLngE7());
-        placeVisitModel.setStartTimestampMs(timeLineObject.getPlaceVisit().getDuration().getStartTimestampMs());
-        placeVisitModel.setEndTimestampMs(timeLineObject.getPlaceVisit().getDuration().getEndTimestampMs());
-        return placeVisitModel;
+    private void extractData(TimeLineObject timeLineObject) {
+        long lat = timeLineObject.getPlaceVisit().getCenterLatE7();
+        long lon = timeLineObject.getPlaceVisit().getCenterLngE7();
+        long timestamp = timeLineObject.getPlaceVisit().getDuration().getStartTimestampMs();
+        long duration = timeLineObject.getPlaceVisit().getDuration().getEndTimestampMs() - timestamp;
+        long updateTime = System.currentTimeMillis();
+
+        locationHistoryRepository.save(new LocationHistory(lat, lon, timestamp, duration, updateTime));
     }
 }
