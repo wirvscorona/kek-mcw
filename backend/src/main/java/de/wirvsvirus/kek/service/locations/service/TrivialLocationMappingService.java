6078e1189f6c9ecac2c16e86490044cf5a831a1b
lenaWitterauf <lena.emma77@gmail.com>
2020-03-21 15:31:38 +0100

    fix compile errors after merge

--- backend/src/main/java/de/wirvsvirus/kek/service/locations/service/TrivialLocationMappingService.java
+++ backend/src/main/java/de/wirvsvirus/kek/service/locations/service/TrivialLocationMappingService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
