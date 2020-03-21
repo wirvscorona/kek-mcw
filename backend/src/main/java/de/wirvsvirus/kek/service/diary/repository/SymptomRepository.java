e3d9a64c917c207f99172d816fdf0c0563151b3a
Florian Breuer <florian.breuer@student.kit.edu>
2020-03-21 18:16:22 +0100

    * expanded symptom + symptom controller * Search endpoint

--- backend/src/main/java/de/wirvsvirus/kek/service/diary/repository/SymptomRepository.java
+++ backend/src/main/java/de/wirvsvirus/kek/service/diary/repository/SymptomRepository.java
@@ -1,9 +1,13 @@
 package de.wirvsvirus.kek.service.diary.repository;
 
+import java.util.Collection;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import de.wirvsvirus.kek.service.diary.model.Symptom;
 
 @Repository
 public interface SymptomRepository extends CrudRepository<Symptom, Long> {
+    Collection<Symptom> findByName(String name);
+    Collection<Symptom> findByNameContains(String name);
 }
