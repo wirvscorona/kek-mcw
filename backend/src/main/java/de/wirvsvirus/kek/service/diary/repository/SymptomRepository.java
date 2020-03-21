a55d583a615ce85c20cd4cf411ab8fe2e0f9bc99
Lorenz Junglas <lolleko@users.noreply.github.com>
2020-03-21 14:01:11 +0100

    Added symptoms repo

---
+++ backend/src/main/java/de/wirvsvirus/kek/service/diary/repository/SymptomRepository.java
@@ -0,0 +1,11 @@
+package de.wirvsvirus.kek.service.diary.repository;
+
+import java.util.Collection;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import de.wirvsvirus.kek.service.diary.model.Symptom;
+
+@Repository
+public interface SymptomRepository extends CrudRepository<Symptom, Long> {
+}
