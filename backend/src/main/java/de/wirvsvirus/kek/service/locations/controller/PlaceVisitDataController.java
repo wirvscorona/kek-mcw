74e145d3565941b52972c761d6c0a16c7bd74a62
eazana4 <38957394+eazana4@users.noreply.github.com>
2020-03-21 12:43:25 +0100

    Added Class PlaceVisitDataController

---
+++ backend/src/main/java/de/wirvsvirus/kek/service/locations/controller/PlaceVisitDataController.java
@@ -0,0 +1,52 @@
+package de.wirvsvirus.kek.service.locations.controller;
+
+import de.wirvsvirus.kek.service.diary.model.Diary;
+import de.wirvsvirus.kek.service.locations.model.pojo.TimeLineObject;
+import de.wirvsvirus.kek.service.locations.model.pojo.TimelineJsonRoot;
+import de.wirvsvirus.kek.service.locations.repository.LocationHistory;
+import de.wirvsvirus.kek.service.locations.repository.PlaceVisitRepository;
+import de.wirvsvirus.kek.service.locations.service.TrivialLocationMappingService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RestController
+@RequestMapping("/")
+public class PlaceVisitDataController {
+
+    @Autowired
+    private PlaceVisitRepository placeVisitRepository;
+
+    @Autowired
+    private TrivialLocationMappingService trivialLocationMappingService;
+
+    @PostMapping("/locations/{user}/upload")
+    @ApiOperation(value = "Responds with a list of diaries, if parameters are set it will respond with a list of contacts taken between start and finish", response = Diary.class)
+    public ResponseEntity<String> uploadLocationData(@RequestBody TimelineJsonRoot jsonData, @PathVariable String user) {
+        List<LocationHistory> locationHistories = jsonData.getTimelineObjects().stream()
+                .filter( timeLineObject -> timeLineObject.getPlaceVisit() != null)
+                .map(this::extractData)
+                .collect(Collectors.toList());
+        placeVisitRepository.saveAll(locationHistories);
+
+
+
+        return new ResponseEntity<String>("Upload success", HttpStatus.OK);
+    }
+
+    private LocationHistory extractData(TimeLineObject timeLineObject) {
+        long lat = timeLineObject.getPlaceVisit().getLocation().;
+        long lon = timeLineObject.getPlaceVisit().getCenterLngE7();
+        long startTimestampMs = timeLineObject.getPlaceVisit().getDuration().getStartTimestampMs();
+        long endTimestampMs = timeLineObject.getPlaceVisit().getDuration().getEndTimestampMs();
+        long updateTime = System.currentTimeMillis();
+
+        return new LocationHistory(lat, lon, startTimestampMs, endTimestampMs, updateTime);
+    }
+}
