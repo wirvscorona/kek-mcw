59296a2b1032ec71c83450f12d6627cae0fad11c
Julia Schmid <julaschmid@googlemail.com>
2020-03-21 11:51:53 +0100

    UI for diary entries

--- frontend/src/Diary.js
+++ frontend/src/Diary.js
@@ -1,13 +1,68 @@
 import React, { Component } from 'react'
-import DiaryTable from './DiaryTable'
+import DiaryTableEntry from './DiaryEntry'
+
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 class Diary extends Component {
     render() {
+        const diaryEntries = []
+        let i = 0
+        while (i < 14) {
+            diaryEntries.push(<div key={i}>< DiaryTableEntry /></div>)
+            i++;
+        }
         return (
-            <div className="container">
-                <DiaryTable />
-            </div>
-    )
+            <Container fluid>
+                <Row>
+                    <Col lg={1}>
+                        &nbsp;
+                        </Col>
+                    <Col lg={2}>
+                        &nbsp;
+                        </Col>
+                    <Col lg={2}>
+                        &nbsp;
+                        </Col>
+                    <Col className="text-center font-bold">
+                        Eigene Symptomatik
+                        </Col>
+                </Row>
+                <Row className="my-4 font-bold">
+                    <Col lg={1}>
+                        Datum
+                        </Col>
+                    <Col lg={2}>
+                        Art des Kontakts
+                        </Col>
+                    <Col lg={2}>
+                        Art des verwendeten Schutzes
+                        </Col>
+                    <Col>
+                        <Container>
+                            <Row className="text-muted">
+                                <Col lg={2}>
+                                    Fieber
+                                    </Col>
+                                <Col lg={2}>
+                                    Husten
+                                    </Col>
+                                <Col lg={2}>
+                                    Halsschmerzen
+                                    </Col>
+                                <Col lg={2}>
+                                    Kurzatmigkeit
+                                    </Col>
+                                <Col lg={2}>
+                                    Sonstige Symptome
+                                    </Col>
+                            </Row>
+                        </Container>
+                    </Col>
+                </Row>
+                {diaryEntries}
+            </Container>
+        )
     }
 }
 
